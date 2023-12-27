1d3592095a3e8a24596a808a3b0c78be0c590e43
Navjeevan Alone <navjeevanalone352@gmail.com>
2023-12-27 22:48:13 +0530

    init

---
+++ script.js
@@ -0,0 +1,58 @@
+
+const wrapper = document.querySelector('.btn-wrapper');
+const title = document.querySelector(".title")
+const bodyElement = document.body;
+
+wrapper.addEventListener('mouseover', function () {
+    moveWrapperToRandomPosition();
+});
+
+function moveWrapperToRandomPosition() {
+    const windowHeight = window.innerHeight - wrapper.offsetHeight;
+    const windowWidth = window.innerWidth - wrapper.offsetWidth;
+    
+    const newX = Math.random() * windowWidth;
+    const newY = Math.random() * windowHeight;
+    
+    wrapper.style.left = newX + 'px';
+    wrapper.style.top = newY + 'px';
+}
+const buttonYes = document.getElementById('yes');
+const image = document.getElementById('gif');
+
+buttonYes.addEventListener('click', () => {
+    image.src = 'https://media1.tenor.com/m/1A6yNLKJIx0AAAAC/hi.gif'; // Update this path to your new image
+    buttonYes.remove();
+    wrapper.remove();
+    title.innerHTML = "Hello you clicked"
+    bodyElement.style.cursor = `url(./assets/huggingface.png), auto`;
+
+}); 
+function disableRightClick() {
+    document.addEventListener('contextmenu', function (event) {
+        event.preventDefault();
+    });
+}
+
+function disableKeyboardShortcuts() {
+    document.addEventListener('keydown', function (event) {
+        // You can add specific key combinations you want to block
+        if (event.ctrlKey || event.shiftKey || event.altKey || event.metaKey) {
+            event.preventDefault();
+        }
+    });
+}
+
+function blockDevTools() {
+    // This is a simple and not very effective way to discourage opening DevTools
+    document.onkeydown = function (e) {
+        if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 'I'.charCodeAt(0) || e.keyCode == 'C'.charCodeAt(0) || e.keyCode == 'J'.charCodeAt(0)))) {
+            alert('Developer tools are blocked on this page');
+            return false;
+        }
+    }
+}
+
+disableRightClick();
+disableKeyboardShortcuts();
+blockDevTools();
